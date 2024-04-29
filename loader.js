d9abc37fa596f6fabecf7e7f5e3c39f8b3752b08
Adodoadjie <eadodoadjie@yahoo.com>
2024-04-29 22:04:09 +0000

    made changes in loader.js

--- loader.js
+++ loader.js
@@ -1,5 +1,5 @@
 "use client";
 
-export default function muyImageLoader({ src, width, quality }) {
+export default function myImageLoader({ src, width, quality }) {
   return "https://emmanuel-portfolio.com/${src?w=${width}&q=${quality || 75}";
 }
