ee57044a6022332b51c911b185793c52faa793a2
Adodoadjie <eadodoadjie@yahoo.com>
2024-04-29 22:08:30 +0000

    changes to loader.js

--- loader.js
+++ loader.js
@@ -1,5 +1,5 @@
 "use client";
 
 export default function myImageLoader({ src, width, quality }) {
-  return "https://emmanueladodoadjie-git-main-ados1s-projects.vercel.app//${src?w=${width}&q=${quality || 75}";
+  return "https://emmanueladodoadjie-oksqfxr57-ados1s-projects.vercel.app/${src?w=${width}&q=${quality || 75}";
 }
